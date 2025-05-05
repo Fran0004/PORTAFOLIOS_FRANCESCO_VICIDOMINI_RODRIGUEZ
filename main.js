8c7fdda9fbe8c8e4b3f0f65b8a3eecd8606a65d2
Fran0004 <116067683+Fran0004@users.noreply.github.com>
2025-05-05 22:07:24 +0200

    backgorund update

--- main.js
+++ main.js
@@ -2,3 +2,22 @@ AOS.init({
     duration: 1000,
     once: true
   });
+
+  const section = document.getElementById('inicio');
+  const footer = document.getElementById("finalShin");
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        footer.classList.remove('activo');
+        footer.classList.add('desctivo');
+      } else {
+        footer.classList.add('activo');
+        footer.classList.remove('desctivo');
+      }
+    });
+  }, {
+    threshold: 0.5 // Cambia si quieres que se active antes o después
+  });
+
+  observer.observe(section);
